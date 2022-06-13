faa983f06bbc730209347a47461f653ce863c9ed
jacobpartin <jakebpartin@gmail.com>
2022-06-13 09:20:59 -0500

    Fixed settings

--- UrlValidator/wsgi.py
+++ UrlValidator/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UrlValidat.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UrlValidator.settings')
 
 application = get_wsgi_application()
