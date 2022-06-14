14921e47e92c5a28ad9c3dea4475903e90f83642
jacobpartin <jakebpartin@gmail.com>
2022-06-14 08:58:11 -0500

    added multiprocessing

--- UrlValidator/views.py
+++ UrlValidator/views.py
@@ -1,28 +1,48 @@
+import hashlib
+import os
+import re
+import time
+from pathlib import Path
+
+from bs4.dammit import EncodingDetector
 from django.http import JsonResponse
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urlunparse
 from urllib.parse import urljoin
+import multiprocessing as mp
 
 
 def get_scrape(request):
     address = request.GET["address"]
-    scrape = Scrape()
-    scrape.find_broken_links(urlparse(address).netloc, address, "")
-    return JsonResponse({"broken_links": scrape.broken_links})
+    job_queue = mp.Queue()
+    manager = mp.Manager()
+    searched = manager.list()
+    broken = manager.list()
+    jobs = []
+    job_queue.put({"url": address, "parent": address})
+    for i in range(os.cpu_count() - 1):
+        p = Worker(job_queue, urlparse(address).netloc, searched, broken)
+        jobs.append(p)
+        p.start()
+    for j in jobs:
+        j.join()
+    return JsonResponse({"broken_links": list(broken)})
 
 
 def index(request):
     return render(request, "urlvalidator/frontpage.html")
 
 
-class Scrape:
+class Worker(mp.Process):
 
-    def __init__(self):
-        self.searched_links = []
-        self.broken_links = []
-        self.count = 0
+    def __init__(self, job_queue, domain, searched, broken):
+        super().__init__()
+        self.job_queue = job_queue
+        self.searched = searched
+        self.broken = broken
+        self.domain = domain
 
     @classmethod
     def get_links_from_html(cls, html):
@@ -39,23 +59,30 @@ class Scrape:
             'status_code': status_code
         }
 
-    def find_broken_links(self, domain_to_search, check_url, parent_url):
-        if (not (check_url in self.searched_links)) and (not check_url.startswith("mailto:")) and (
-                not ("javascript:" in check_url)) and (
-                not check_url.endswith(".png")) and (not check_url.endswith(".jpg")) and (
-                not check_url.endswith(".jpeg")):
-            try:
-                request_obj = requests.get(check_url, timeout=5, headers={
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
-                })
-
-                self.searched_links.append(check_url)
-                if request_obj.status_code > 299:
-                    self.broken_links.append(self.link_to_obj(check_url, parent_url, request_obj.status_code))
-                    self.count += 1
-                else:
-                    if urlparse(check_url).netloc == domain_to_search:
-                        for link in self.get_links_from_html(request_obj.text):
-                            self.find_broken_links(domain_to_search, urljoin(check_url, link), check_url)
-            except Exception:
-                self.searched_links.append(domain_to_search)
+    def run(self):
+        while True:
+            if self.job_queue.empty():
+                time.sleep(3)
+                if self.job_queue.empty():
+                    break
+            queue = self.job_queue.get()
+            url = queue["url"]
+            parent = queue["parent"]
+            if (not (url in self.searched)) and (not url.startswith("mailto:")) and (
+                    not ("javascript:" in url)) and (
+                    not url.endswith(".png")) and (not url.endswith(".jpg")) and (
+                    not url.endswith(".jpeg")):
+                self.searched.append(url)
+                try:
+                    page = requests.get(url, timeout=3, headers={
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
+                    })
+                    if page.status_code > 299:
+                        self.broken.append(self.link_to_obj(url, parent, page.status_code))
+                    else:
+                        if urlparse(url).netloc == self.domain:
+                            for link in self.get_links_from_html(page.text):
+                                self.job_queue.put({"url": urljoin(url, link), "parent": url})
+                except requests.RequestException:
+                    pass
+        self.close()
